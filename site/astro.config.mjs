7d5a3e28effaef9bae924d6be82fe18dda4b08fe
freyalilzy <freyalilzy@gmail.com>
2026-01-19 11:57:16 +0000

    Update astro.config.mjs

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://pixel-87.github.io',
+    site: 'https://freyalilzy.github.io',
     base: '/port',
 });
